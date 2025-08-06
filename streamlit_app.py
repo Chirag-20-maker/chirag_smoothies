095da750ea845f1284f3c858cc6f667380fb6fa1
Chirag-20-maker <wadhwanichirag20@gmail.com>
2025-08-06 15:36:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,19 +12,19 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_Name'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
+name_on_order = st.text_input("Name on Smoothie:")
+st.write("The name on Smoothie will be", name_on_order)
+
+
 ingredients_list = st.multiselect(
     "Choose upto 5 ingredients",
      my_dataframe, 
     max_selections = 5
 )
 
-
 if ingredients_list:
 
     ingredients_string = ''
-    name_on_order = st.text_input("Name on Smoothie:")
-    st.write("The name on Smoothie will be", name_on_order)
-
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
     
